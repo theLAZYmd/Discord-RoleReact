5e69f53329a84328557067110eebd10fc349e163
theLAZYmd <lazymeep+github@gmail.com>
2019-07-30 22:00:00 +0100

    Linted and fixed a few mistypes

--- role.js
+++ role.js
@@ -1,4 +1,3 @@
-/* eslint-disable*/
 // Import constructors, configuration and login the client
 const { Client, RichEmbed, Emoji, MessageReaction } = require('discord.js');
 const CONFIG = require('./config');
@@ -6,17 +5,17 @@ const PACKAGE = require('./package.json');
 
 const client = new Client({ disableEveryone: true });
 if (CONFIG.botToken === '')
-    throw new Error("The 'botToken' property is not set in the config.js file. Please do this!");
+	throw new Error('The \'botToken\' property is not set in the config.js file. Please do this!');
 
 // Client events to let you know if the bot is online and to handle any Discord.js errors
-client.on("ready", () => {
-	console.log("Role Reactions is online!");
-	if (Array.isArray(CONFIG.roles) || Array.isArray(CONFIG.reactions)) throw `Config with .roles and .reactions is deprecated in ${PACKAGE.version} of the bot. Please review new CONFIG format.`
+client.on('ready', () => {
+	console.log('Role Reactions is online!');
+	if (Array.isArray(CONFIG.roles) || Array.isArray(CONFIG.reactions)) throw `Config with .roles and .reactions is deprecated in ${PACKAGE.version} of the bot. Please review new CONFIG format.`;
 });
 client.on('error', console.error);
 
 // Handles the creation of the role reactions. Will either send the role messages separately or in an embed
-client.on("message", message => {
+client.on('message', message => {
 	try {
 
 		// Make sure bots can't run this command
@@ -37,19 +36,19 @@ client.on("message", message => {
 		// Check: can the bot delete messages? If not that's an error
 		if (CONFIG.deleteSetupCMD) {
 			if (!message.channel.memberPermissions(message.guild.me).has('MANAGE_MESSAGES'))
-				throw new Error("I need permission to delete your command message! Please assign the 'Manage Messages' permission to me in this channel!");
+				throw new Error('I need permission to delete your command message! Please assign the \'Manage Messages\' permission to me in this channel!');
 			message.delete().catch(()=>{});
 		}
 
 		// Check: can the bot add reactions? If not that's an error
 		if (!message.channel.memberPermissions(message.guild.me).has('ADD_MESSAGES'))
-			throw new Error("I need permission to add reactions to these messages! Please assign the 'Add Reactions' permission to me in this channel!");
+			throw new Error('I need permission to add reactions to these messages! Please assign the \'Add Reactions\' permission to me in this channel!');
 
-		const argument = message.content.slice(CONFIG.setupCMD.length).trim();
+		let argument = message.content.slice(CONFIG.setupCMD.length).trim();
 		if (argument && typeof argument !== 'number')
 			throw new Error('Argument parameter must be a number!');
 		argument++;
-		if (typeof config['embed' + argument.toString()] !== 'object')
+		if (typeof CONFIG['embed' + argument.toString()] !== 'object')
 			throw new Error('Couldn\'t find list of emojis/roles with that embed index');
 
 		// Does the user want the response to be in the form of a large embed?
@@ -59,7 +58,7 @@ client.on("message", message => {
 
 			// All well and good that the user wants a message but have they set their initial message?
 			if (!CONFIG.initialMessage || (CONFIG.initialMessage === '')) 
-				throw new Error("The 'initialMessage' property is not set in the config.js file. Please do this!");
+				throw new Error('The \'initialMessage\' property is not set in the config.js file. Please do this!');
 
 			message.channel.send(CONFIG.initialMessage);
 
@@ -77,11 +76,11 @@ client.on("message", message => {
 
 			// All well and good that the user wants an embed but have they filled in the properties?
 			if (!CONFIG.embedMessage)
-				throw new Error("The 'embedMessage' property is not set in the config.js file. Please do this!");
+				throw new Error('The \'embedMessage\' property is not set in the config.js file. Please do this!');
 			if (!CONFIG.embedFooter)
-				throw new Error("The 'embedFooter' property is not set in the config.js file. Please do this!");
+				throw new Error('The \'embedFooter\' property is not set in the config.js file. Please do this!');
 			if (!CONFIG.embedColor)
-				throw new Error("The 'embedColor' property is not set in the config.js file. Please do this!");
+				throw new Error('The \'embedColor\' property is not set in the config.js file. Please do this!');
 
 			let fields = CONFIG['embed' + argument.toString()];
 
@@ -91,7 +90,7 @@ client.on("message", message => {
 				.setColor(CONFIG.embedColor)
 				.setThumbnail(CONFIG.embedThumbnailLink || message.guild.iconURL || undefined);
 
-			if (fields.length > 25) throw new Error("That maximum roles that can be set for an embed is 25!");
+			if (fields.length > 25) throw new Error('That maximum roles that can be set for an embed is 25!');
 			for (let [emoji, role] of fields) {
 				if (!message.guild.roles.find(r => r.name === role))
 					throw new Error(`The role '${role}' does not exist!`);
@@ -114,7 +113,7 @@ client.on("message", message => {
 		if (CONFIG.embed) message.channel.send({
 			color: CONFIG.embedErrorColor,
 			description: e.message
-		})
+		});
 		else message.channel.send(e.message);
 	}
 });
@@ -127,37 +126,37 @@ const events = {
 
 // This event handles adding/removing users from the role(s) they chose based on message reactions
 client.on('raw', async event => {
-    if (!events.hasOwnProperty(event.t)) return;
+	if (!events.hasOwnProperty(event.t)) return;
 	const { d: data } = event;
 	
 	// Generate properties usable for the rest of the function
-    const user = client.users.get(data.user_id);
-    const message = await channel.fetchMessage(data.message_id);
-    const channel = client.channels.get(data.channel_id);
+	const user = client.users.get(data.user_id);
+	const message = await channel.fetchMessage(data.message_id);
+	const channel = client.channels.get(data.channel_id);
 	const member = message.guild.members.get(user.id);
 	// Checks for when not to react
 	if (user.bot) return;
 	if (message.author.id !== client.user.id) return;
 	if (channel.memberPremissions(client.guild.me).has('ADD_MESSAGES')) return;
 	// Generate a 'reaction' object
-    const emojiKey = (data.emoji.id) ? `${data.emoji.name}:${data.emoji.id}` : data.emoji.name;
-    let reaction = message.reactions.get(emojiKey);
-    if (!reaction) {
-        const emoji = new Emoji(client.guilds.get(data.guild_id), data.emoji);
-        reaction = new MessageReaction(message, emoji, 1, data.user_id === client.user.id);
+	const emojiKey = (data.emoji.id) ? `${data.emoji.name}:${data.emoji.id}` : data.emoji.name;
+	let reaction = message.reactions.get(emojiKey);
+	if (!reaction) {
+		const emoji = new Emoji(client.guilds.get(data.guild_id), data.emoji);
+		reaction = new MessageReaction(message, emoji, 1, data.user_id === client.user.id);
 	}
 	
 	let roleName = CONFIG.ROLES[reaction.emoji.name];
 	if (!roleName) return;
-	let guildRole = message.guild.roles.find(r => r.name === value);
+	let guildRole = message.guild.roles.find(r => r.name === roleName);
 	if (!guildRole) return;
-	if (event.t === "MESSAGE_REACTION_ADD") member.addRole(guildRole.id);
-	else if (event.t === "MESSAGE_REACTION_REMOVE") member.removeRole(guildRole.id);
+	if (event.t === 'MESSAGE_REACTION_ADD') member.addRole(guildRole.id);
+	else if (event.t === 'MESSAGE_REACTION_REMOVE') member.removeRole(guildRole.id);
 });
 
 process.on('unhandledRejection', err => {
-    const msg = err.stack.replace(new RegExp(`${__dirname}/`, 'g'), './');
-	console.error("Unhandled Rejection", msg);
+	const msg = err.stack.replace(new RegExp(`${__dirname}/`, 'g'), './');
+	console.error('Unhandled Rejection', msg);
 });
 
 
