4549f0933d0ae6487e44ff65086f7f1c0b385226
theLAZYmd <lazymeep+github@gmail.com>
2019-07-30 21:42:52 +0100

    Moved fields object to be declared once

--- role.js
+++ role.js
@@ -60,25 +60,29 @@ client.on("message", message => {
 		if (!message.channel.memberPermissions(message.guild.me).has('ADD_MESSAGES'))
 			throw new Error("I need permission to add reactions to these messages! Please assign the 'Add Reactions' permission to me in this channel!");
 
+		const fields = Object.entries(CONFIG.roles);
+
 		// Does the user want the response to be in the form of a large embed?
 		if (!CONFIG.embed) {
+
+			// All well and good that the user wants a message but have they set their initial message?
 			if (!CONFIG.initialMessage || (CONFIG.initialMessage === '')) 
 				throw new Error("The 'initialMessage' property is not set in the config.js file. Please do this!");
 
 			message.channel.send(CONFIG.initialMessage);
 
-			const messages = generateMessages();
-			for (const { role, message: msg, emoji } of messages) {
+			for (let [emoji, role] of fields) {
 				if (!message.guild.roles.find(r => r.name === role))
 					throw `The role '${role}' does not exist!`;
 
-				message.channel.send(msg).then(async m => {
+				message.channel.send(`React below to get the **"${role}"** role!`).then(async m => {
 					let customCheck = message.guild.emojis.find(e => e.name === emoji);
 					if (!customCheck) await m.react(emoji);
 					else await m.react(customCheck.id);
 				}).catch(console.error);
 			}
 		} else {
+			
 			// All well and good that the user wants an embed but have they filled in the properties?
 			if (!CONFIG.embedMessage)
 				throw new Error("The 'embedMessage' property is not set in the config.js file. Please do this!");
@@ -93,7 +97,6 @@ client.on("message", message => {
 				.setColor(CONFIG.embedColor)
 				.setThumbnail(CONFIG.embedThumbnailLink || message.guild.iconURL || undefined);
 
-			const fields = Object.entries(CONFIG.roles);
 			if (fields.length > 25) throw new Error("That maximum roles that can be set for an embed is 25!");
 			for (let [emoji, role] of fields) {
 				if (!message.guild.roles.find(r => r.name === role))
